b789fc61fd38bacb095864990fbb1991da4db558
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-20 17:37:50 -0400

    Added to get /:id & comments for myself

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,8 +7,8 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tags = await Tag.findAll ({
-      include: [Product]
+    const tags = await Tag.findAll({
+      include: [Product], //Do I add more here?
     });
     res.json(tags);
   } catch (err) {
@@ -16,9 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [Product], //Do I add more here?
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'Tag not found.' });
+      return;
+    }
+    res.json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
